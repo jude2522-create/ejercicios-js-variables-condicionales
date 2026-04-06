1bfb6acdd5aea3642f5855d4835d930601a9fd8b
Judith <jude2522@gmail.com>
2026-04-06 16:49:50 -0300

    ejercicio8 resuelto

---
+++ ejercicios/08-ejercicio.js
@@ -0,0 +1,7 @@
+const num = parseInt(prompt("Ingresá un número:"));
+
+if (num % 2 === 0) {
+    document.write("El " + num + " es divisible por 2.");
+} else {
+    document.write("El " + num + " no es divisible por 2.");
+}
